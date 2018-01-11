9ff4f6f32fd5e2818df5acf1eb9611ffc82fd554
Benjamin Roland Buch <benni.buch@gmail.com>
2018-01-11 18:03:28 +0100

    update copyright

--- include/http/websocket_server_request_parser.hpp
+++ include/http/websocket_server_request_parser.hpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// Copyright (c) 2012-2017 Benjamin Buch
+// Copyright (c) 2012-2018 Benjamin Buch
 //
 // https://github.com/bebuch/http
 //
